38a6fcd4984a7a102cd23eb6117088751519c128
Natacha Tavares <natacha.alves@eaportal.org>
2025-05-24 21:50:37 -0300

    criação da função para mostrar os itens da lista e criação da função para marcar os itens como concluidos

--- script.js
+++ script.js
@@ -14,5 +14,36 @@ function addItem(){
 
     document.querySelector("#item").value = ""
 
-    console.log(items)
+    showItemsList()
+}
+
+function showItemsList(){
+    const sectionList = document.querySelector(".list")
+
+    sectionList.innerHTML = ""
+    
+    items.sort((itemA, itemB) => Number(itemA.checked) - Number(itemB.checked))
+
+    items.map((item, index) => {
+        sectionList.innerHTML += `
+            <div class="item">
+                <div>
+                    <input type="checkbox" name="list" id="item-${index}">
+                    <div class="custom-checkbox">
+                        <img src="./assets/checked (1).svg" alt="checked">
+                    </div>
+                    <label for="item-${index}">${item.name}</label>
+                </div>
+                <button>
+                    <img src="./assets/trash-icon (1).svg" alt="trash icon">
+                </button>
+            </div>
+        `
+    })
+}
+
+function checkItem(itemName){
+    const item = items.find((item) => item.name === itemName)
+    item.checked = !item.checked
+    showItemsList()
 }
