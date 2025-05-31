47bf4f557c77e471e3bee9de9bff6e3670465433
Natacha Tavares <natacha.alves@eaportal.org>
2025-05-31 17:00:07 -0300

    criação da função para marcar os itens e adição do evento de click no label

--- script.js
+++ script.js
@@ -28,11 +28,11 @@ function showItemsList(){
         sectionList.innerHTML += `
             <div class="item">
                 <div>
-                    <input type="checkbox" name="list" id="item-${index}">
+                    <input type="checkbox" name="list" id="item-${index}" ${item.checked === true ? "checked" : ""}>
                     <div class="custom-checkbox">
                         <img src="./assets/checked (1).svg" alt="checked">
                     </div>
-                    <label for="item-${index}">${item.name}</label>
+                    <label for="item-${index}" onclick="checkItem('${item.name}')">${item.name}</label>
                 </div>
                 <button>
                     <img src="./assets/trash-icon (1).svg" alt="trash icon">
